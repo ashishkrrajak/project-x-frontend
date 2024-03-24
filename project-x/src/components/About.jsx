2746ad9013dcc27b58dd61c1b4e7acfad9620b2d
Ashish Kumar Rajak <ashish.kumarrajak765@gmail.com>
2024-03-24 20:55:44 +0530

    added bootstrap and make fixes

--- project-x/src/components/About.jsx
+++ project-x/src/components/About.jsx
@@ -5,6 +5,9 @@ function About(){
     return (
         <>
             <h1>About Page</h1>
+            <div className="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+  <div className="progress-bar w-75"></div>
+</div>
         </>
     )
 }
